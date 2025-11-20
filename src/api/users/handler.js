d1e248d367cc03ab050c47bee20228ce3449a155
DarilInsanKamil <darilinsankamil2132@gmail.com>
2025-11-20 21:04:30 +0700

    feat: add authentication routes and handlers
    - Implemented AuthenticationsHandler with methods for handling authentication requests (POST, PUT, DELETE).
    - Created routes for authentication endpoints.
    - Added AuthenticationsService for managing refresh tokens in the database.
    - Integrated token management for access and refresh tokens.
    - Updated UsersService to include a method for retrieving user by ID.
    - Commented out unused user creation handler and route.
    - Added TensorFlow.js as a dependency in package.json.

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -2,23 +2,30 @@ class UsersHandler {
     constructor(service, validator) {
         this._service = service
         this._validator = validator
-        this.postUserHandler = this.postUserHandler.bind(this);
+        // this.postUserHandler = this.postUserHandler.bind(this);
         this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
     }
-    async postUserHandler(request, h) {
+    // async postUserHandler(request, h) {
+    //     con
 
-    }
+    // }
     async getUserByIdHandler(request, h) {
-        const response = h.response({
-            status: 'success',
-            message: 'User berhasil ditambahkan',
-            data: {
-                'data': 'hello world'
-            }
-        })
+        try {
+            const { id } = request.params;
+            const user = await this._service.getUserById(id);
+            const response = h.response({
+                status: 'success',
+                data: {
+                    user
+                }
+            })
 
-        response.code(201)
-        return response
+            response.code(201)
+            return response
+        } catch (err) {
+            console.log('error: ', err)
+            throw err;
+        }
     }
 }
 
--- src/service/UsersService.js
+++ src/service/UsersService.js
@@ -4,6 +4,20 @@ class UsersService {
     constructor() {
         this._pool = new Pool();
     }
+
+    async getUserById(id) {
+        const query = {
+            text: 'SELECT * from users WHERE id = $1',
+            values: [id]
+        }
+
+        const result = await this._pool.query(query);
+        if (!result.rows.length) {
+            throw new NotFounderror('User tidak ditemukan');
+        }
+
+        return result.rows[0];
+    }
 }
 
 module.exports = UsersService
